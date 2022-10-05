4ebd6f09853a289beab69a3104d685727837c65b
-k <slowdive@me.com>
2022-10-05 12:24:34 -0700

    Add emacs

---
+++ .doom.d/config.el
@@ -0,0 +1,16 @@
+;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
+
+(setq user-full-name "John Doe"
+      user-mail-address "john@doe.com")
+
+(setq doom-theme 'doom-one)
+(setq display-line-numbers-type t)
+(setq org-directory "~/org/")
+(setq confirm-kill-emacs nil)
+
+(setq +doom-dashboard-menu-sections (cl-subseq +doom-dashboard-menu-sections 0 3))
+
+(global-set-key (kbd "C-c c") #'clipboard-kill-ring-save)
+(global-set-key (kbd "C-c v") #'clipboard-yank)
+
+;;(add-hook 'window-setup-hook #'treemacs 'append)
---
+++ .doom.d/packages.el
@@ -0,0 +1,4 @@
+;; -*- no-byte-compile: t; -*-
+;;; $DOOMDIR/packages.el
+
+(package! gnu-apl-mode)
