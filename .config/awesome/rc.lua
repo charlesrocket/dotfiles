0485bd177da0b6b496d0e6fcd854ef380f208917
-k <slowdive@me.com>
2022-10-11 22:26:01 -0700

    Update rules
    Firefox/Emacs

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -493,9 +493,11 @@ awful.rules.rules = {
       }, properties = { titlebars_enabled = false }
     },
 
-    -- Set Firefox to always map on the tag named "2" on screen 1.
-    -- { rule = { class = "Firefox" },
-    --   properties = { screen = 1, tag = "2" } },
+    { rule = { class = "Firefox" },
+      properties = { screen = 1, tag = "3" } },
+
+    { rule = { class = "Emacs" },
+      properties = { screen = 1, tag = "4", maximized = true } },
 }
 -- }}}
 
