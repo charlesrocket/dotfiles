589fee916ea03514f92bf55b79f5bcfc2dde14eb
-k <slowdive@me.com>
2023-04-18 10:04:59 -0700

    Enable zig

--- .config/doom/init.el
+++ .config/doom/init.el
@@ -160,7 +160,7 @@
        ;;terra             ; Earth and Moon in alignment for performance.
        ;;web               ; the tubes
        yaml              ; JSON, but readable
-       ;;zig               ; C, but simpler
+       zig               ; C, but simpler
 
        :email
        ;;(mu4e +org +gmail)
