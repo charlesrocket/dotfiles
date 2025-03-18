94df6bfc6ccc1c64d11dbb9acea905e2a49777fb
-k <slowdive@me.com>
2025-03-18 00:23:06 -0400

    Disable LaTeX spellcheck

--- .config/doom/config.el
+++ .config/doom/config.el
@@ -1,26 +1,22 @@
 ;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
 
 (setq user-full-name "-k"
-      user-mail-address "slowdive@me.com")
-
-(setq doom-theme '
-      doom-tomorrow-night
-      ;;doom-one
-)
-
-(setq display-line-numbers-type t)
-(setq org-directory "~/org/")
-(setq confirm-kill-emacs nil)
-(setq projectile-project-search-path '(("~/src" . 1)))
-;;; (setq default-directory "~/src/dotfiles" )
-
-(setq +doom-dashboard-menu-sections
+      user-mail-address "slowdive@me.com"
+      display-line-numbers-type t
+      org-directory "~/org/"
+      confirm-kill-emacs nil
+      projectile-project-search-path '(("~/src" . 1))
+;;;   default-directory "~/src/dotfiles"
+      doom-theme 'doom-tomorrow-night
+      +doom-dashboard-menu-sections
       (cl-subseq +doom-dashboard-menu-sections 0 3))
 
 (global-set-key (kbd "C-c c") #'clipboard-kill-ring-save)
 (global-set-key (kbd "C-c v") #'clipboard-yank)
 
 ;;; (add-hook 'window-setup-hook #'treemacs 'append)
+
+(setq-hook! 'LaTeX-mode-hook +spellcheck-immediately nil)
 
 (use-package languagetool
   :ensure t
