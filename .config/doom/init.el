233f4cfa3e282d39c2d05d424e8f56659c897417
-k <slowdive@me.com>
2026-01-14 14:30:12 -0500

    Enable QT

--- .config/doom/init.el
+++ .config/doom/init.el
@@ -146,7 +146,7 @@
        ;;graphviz          ; diagrams for confusing yourself even more
        ;;purescript        ; javascript, but functional
        python            ; beautiful is better than ugly
-       ;;qt                ; the 'cutest' gui framework ever
+       qt                ; the 'cutest' gui framework ever
        ;;racket            ; a DSL for DSLs
        ;;raku              ; the artist formerly known as perl6
        ;;rest              ; Emacs as a REST client
