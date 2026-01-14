6f28b844a098b951fc4b7c1645e642b022117dbb
-k <slowdive@me.com>
2026-01-14 14:28:51 -0500

    Enable haskell

--- .config/doom/init.el
+++ .config/doom/init.el
@@ -123,7 +123,7 @@
        ;;gdscript          ; the language you waited for
        ;;(go +lsp)         ; the hipster dialect
        ;;(graphql +lsp)    ; Give queries a REST
-       ;;(haskell +lsp)    ; a language that's lazier than I am
+       (haskell +lsp)    ; a language that's lazier than I am
        ;;hy                ; readability of scheme w/ speed of python
        ;;idris             ; a language you can depend on
        json              ; At least it ain't XML
